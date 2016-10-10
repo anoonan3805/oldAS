fab0c28f29370483e7f96bdd8b2ed24c43c29357
Asha <asha@softstackfactory.org>
2016-10-10 17:40:19 +0000

    HTML

--- www/js/SSFConfig.js
+++ www/js/SSFConfig.js
@@ -33,7 +33,7 @@ angular.module('SSFConfig', [])
 .config(['SSFConfigConstants', function(SSFConfigConstants) {
     SSFConfigConstants['EndpointUrl'] = {
 
-     'url': 'https://promus-production-jbrownssf.c9users.io:8080/api/'
+     'url': 'https://anysession-anoonan3805.c9users.io:8080/api/'
     };
 }])
 .config(['SSFConfigConstants', function(SSFConfigConstants) {
